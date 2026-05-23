079918fe41bb758723750c9b0229e3b31f5d1cb7
Pavel Fiala <pavelfpl@gmail.com>
2026-05-23 15:59:40 +0200

    Update burst_scheduler block - added customization params

--- python/burst_scheduler.py
+++ python/burst_scheduler.py
@@ -3,7 +3,7 @@ from gnuradio import gr
 import pmt
 import pdulib
 class burst_scheduler(gr.sync_block):
-    def __init__(self):
+    def __init__(self,barrier=10000,slot=1000):
         gr.sync_block.__init__(
             self,
             name="burst_scheduler",
@@ -16,18 +16,19 @@ class burst_scheduler(gr.sync_block):
         self.message_port_register_out(pmt.intern("sched_pdu"));
         self.set_msg_handler(pmt.intern("sched_pdu"), self.sched_pdu);
         self.set_msg_handler(pmt.intern("nproduced"), self.nproduced);
+        (self.barrier, self.slot) = (barrier, slot);
         self.sched_barrier = 0;
 
     def sched_pdu(self, pdu):
-        sched_time = (self.nproduced_val + 10000); # pick a time in the future
+        sched_time = (self.nproduced_val + self.barrier);                 # Pick a time in the future - default was 10000 ...
         if(sched_time < self.sched_barrier):
             sched_time = self.sched_barrier;
             print "delaying packet to sched barrier"
 
-        sched_time = sched_time - sched_time%1000; # round to nearest slot
+        sched_time = sched_time - sched_time%self.slot;                   # Round to nearest slot - default was 1000 ...
         event_length = pmt.length(pmt.cdr(pdu));
-        #event_length = pmt.to_long(pmt.dict_ref(pmt.car(pdu), pmt.intern("event_length"), pmt.PMT_NIL));
-        self.sched_barrier = sched_time + event_length + 1000;
+        # event_length = pmt.to_long(pmt.dict_ref(pmt.car(pdu), pmt.intern("event_length"), pmt.PMT_NIL));
+        self.sched_barrier = sched_time + event_length + self.slot;       # Default was 1000 ...
         print "SCHED_EVENT: time=%d, len=%d  "%(sched_time, event_length);
 
         pdu = pdulib.pdu_arg_add(pdu, pmt.intern("event_time"), pmt.from_uint64(sched_time));
